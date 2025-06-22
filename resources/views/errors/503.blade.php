233db3607c6b488230073ab8d5e8e8fc02ac84c7
Zillemudasar2158 <zillemudasar60@gmail.com>
2025-06-22 15:07:12 +0500

    update error file

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" class="h-full bg-gray-100 dark:bg-gray-900">
 <head>
     <meta charset="UTF-8">
-    <title>Site Under Maintenance1</title>
+    <title>Site Under Maintenance</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- TailwindCSS -->
     <script src="https://cdn.tailwindcss.com"></script>
